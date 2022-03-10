14ac1c4cc2c9d195f87290a594155ded40aa0216
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-10 08:06:46 +0700

    [SignupActivity - Back-end Code]: Create account with Firebase

--- app/src/main/java/com/example/prm392_project/SignUpActivity.java
+++ app/src/main/java/com/example/prm392_project/SignUpActivity.java
@@ -1,26 +1,76 @@
 package com.example.prm392_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+    //Views
     EditText editTextEmail;
     EditText editTextPassword;
+    EditText editTextConfirmPassword;
+
+    //Firebase
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //get views
         editTextEmail = findViewById(R.id.editTextEmail2);
         editTextPassword = findViewById(R.id.editTextPassword2);
+        editTextConfirmPassword = findViewById(R.id.editTextConfirmPassword);
+
+        //create firebase
+        mAuth = FirebaseAuth.getInstance();
     }
     public void btnSignUpClick(View view) {
+        //Get data from editText
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+        String confirmPassword = editTextConfirmPassword.getText().toString();
 
+        //Check value empty
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password) || TextUtils.isEmpty(confirmPassword)) {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+        }
+        //check confirm password
+        else if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Confirm password does not match", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            //create account with Firebase
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    //check result
+                    if (task.isSuccessful()) {
+                        //success: notify & go to Login Activity
+                        Toast.makeText(SignUpActivity.this, "User registered", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+                        startActivity(i);
+                        //close this activity
+                        finish();
+                    } else {
+                        //fail: notify
+                        Toast.makeText(SignUpActivity.this, "Fail to register user", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 
     public void btnLoginInsteadClick(View view) {
