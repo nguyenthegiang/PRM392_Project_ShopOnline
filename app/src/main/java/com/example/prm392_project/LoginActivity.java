d27bdc33ad8a72cadbbf9d71a9b5a8d2384e2d76
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-10 19:14:40 +0700

    [LoginActivity - BackEnd]: Auto login

--- app/src/main/java/com/example/prm392_project/LoginActivity.java
+++ app/src/main/java/com/example/prm392_project/LoginActivity.java
@@ -5,19 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+    //Views
     EditText editTextEmail;
     EditText editTextPassword;
+    Button btnLogin;
+    ProgressBar progressBar;
+
+    //Firebase
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //get Views
         editTextEmail = findViewById(R.id.editTextEmail);
         editTextPassword = findViewById(R.id.editTextPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        progressBar = findViewById(R.id.progressBarLogin);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Check if user is already logged in -> move to MainActivity
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            this.finish();
+        }
     }
 
     public void btnLoginClick(View view) {
