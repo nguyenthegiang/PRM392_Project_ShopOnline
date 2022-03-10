fdd57d89c83847cdb04d0e3371ccd8cd0fa85159
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-10 22:13:50 +0700

    [AddProductActivity - BackEnd]: Get views

--- app/src/main/java/com/example/prm392_project/AddProductActivity.java
+++ app/src/main/java/com/example/prm392_project/AddProductActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class AddProductActivity extends AppCompatActivity {
+    //Views
+    EditText editTextName, editTextPrice, editTextDescription, editTextImageURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        //Get Views
+        editTextName = findViewById(R.id.editTextName);
+        editTextPrice = findViewById(R.id.editTextPrice);
+        editTextDescription = findViewById(R.id.editTextDescription);
+        editTextImageURL = findViewById(R.id.editTextImageURL);
     }
 
     public void onCreateClick(View view) {
