68b94594563e21c3cbeedc4eed662dbb99121b73
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-10 22:26:52 +0700

    [AddProductActivity - BackEnd]: Init Firebase

--- app/src/main/java/com/example/prm392_project/AddProductActivity.java
+++ app/src/main/java/com/example/prm392_project/AddProductActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AddProductActivity extends AppCompatActivity {
     //Views
     EditText editTextName, editTextPrice, editTextDescription, editTextImageURL;
+    ProgressBar progressBar;
+    Button btnSave;
+
+    //Firebase
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+    String productId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,12 @@ public class AddProductActivity extends AppCompatActivity {
         editTextPrice = findViewById(R.id.editTextPrice);
         editTextDescription = findViewById(R.id.editTextDescription);
         editTextImageURL = findViewById(R.id.editTextImageURL);
+        progressBar = findViewById(R.id.progressBarAddProduct);
+        btnSave = findViewById(R.id.btnSave);
+
+        //Firebase
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("Products");
     }
 
     public void onCreateClick(View view) {
