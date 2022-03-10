b7d3994a3bac5d1407a14f378983774e542b0992
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-10 21:35:58 +0700

    [MainActivity - FrontEnd]: Add Button

--- app/src/main/java/com/example/prm392_project/MainActivity.java
+++ app/src/main/java/com/example/prm392_project/MainActivity.java
@@ -9,11 +9,14 @@ import android.widget.EditText;
 
 /*Product List*/
 public class MainActivity extends AppCompatActivity {
-;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void moveToAddProduct(View view) {
+        
     }
 }
