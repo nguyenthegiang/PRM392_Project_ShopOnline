768bb7df001d4e910531308a2bbfd993bf10992d
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-10 19:26:01 +0700

    [LoginActivity - BackEnd]: Login with Firebase

--- app/src/main/java/com/example/prm392_project/LoginActivity.java
+++ app/src/main/java/com/example/prm392_project/LoginActivity.java
@@ -1,14 +1,20 @@
 package com.example.prm392_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -49,8 +55,40 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
+    //Login
     public void btnLoginClick(View view) {
+        //show Progress Bar
+        progressBar.setVisibility(View.VISIBLE);
 
+        //get input values
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+        //Validation
+        if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //login with Firebase
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                //hide progress bả
+                progressBar.setVisibility(View.INVISIBLE);
+                //check result
+                if (task.isSuccessful()) {
+                    //Success: notify & move to MainActivity, close this one
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    //Fail: notify
+                    Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void btnSignUpInsteadClick(View view) {
