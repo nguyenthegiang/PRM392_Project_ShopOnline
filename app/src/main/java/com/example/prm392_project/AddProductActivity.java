886bec84b1fd11f978ad0283a69d498df2146953
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-10 22:11:53 +0700

    [AddProductActivity - FrontEnd]: Create views

--- app/src/main/java/com/example/prm392_project/AddProductActivity.java
+++ app/src/main/java/com/example/prm392_project/AddProductActivity.java
@@ -3,6 +3,7 @@ package com.example.prm392_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class AddProductActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class AddProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+    }
+
+    public void onCreateClick(View view) {
+
     }
 }
