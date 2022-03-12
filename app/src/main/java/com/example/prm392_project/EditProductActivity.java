77bca4b8e60fdec980728cf65aeda67d4490e681
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-12 09:53:14 +0700

    [EditProductActivity - BackEnd]: Get chosen Product & display to EdtiText

--- app/src/main/java/com/example/prm392_project/EditProductActivity.java
+++ app/src/main/java/com/example/prm392_project/EditProductActivity.java
@@ -3,12 +3,50 @@ package com.example.prm392_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class EditProductActivity extends AppCompatActivity {
+    //Views
+    private EditText editTextName, editTextPrice, editTextDescription, editTextImageURL;
+    private ProgressBar loadingPB;
+    //Firebase
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    //Model
+    ProductModel productModel;
+    private String productID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_product);
+        //get Views
+        editTextName = findViewById(R.id.editTextNameEdit);
+        editTextPrice = findViewById(R.id.editTextPriceEdit);
+        editTextDescription = findViewById(R.id.editTextDescriptionEdit);
+        editTextImageURL = findViewById(R.id.editTextImageURLEdit);
+        loadingPB = findViewById(R.id.progressBarEditProduct);
+
+        //get the model passed from MainActivity
+        productModel = getIntent().getParcelableExtra("product");
+
+        //set data of chosen product to editText
+        if (productModel != null) {
+            editTextName.setText(productModel.getName());
+            editTextPrice.setText(productModel.getPrice());
+            editTextDescription.setText(productModel.getDescription());
+            editTextImageURL.setText(productModel.getImageURL());
+            productID = productModel.getId();
+        }
+
+        //get database reference to the chosen product
+        databaseReference = firebaseDatabase.getReference("Products").child(productID);
     }
+
+    //Event click [Update] button
 }
--- app/src/main/java/com/example/prm392_project/ProductModel.java
+++ app/src/main/java/com/example/prm392_project/ProductModel.java
@@ -58,6 +58,14 @@ public class ProductModel implements Parcelable {
         this.imageURL = imageURL;
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
