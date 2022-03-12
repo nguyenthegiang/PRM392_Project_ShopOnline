755076ee68e5723457a2e28a7d863f2fa030361b
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-12 22:35:36 +0700

    [ProductDetailActivity - BackEnd]: finish

--- app/src/main/java/com/example/prm392_project/ProductDetailActivity.java
+++ app/src/main/java/com/example/prm392_project/ProductDetailActivity.java
@@ -3,15 +3,44 @@ package com.example.prm392_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class ProductDetailActivity extends AppCompatActivity {
+    //Views
+    private TextView textViewPriceDetail, textViewDescriptionDetail;
+    private ImageView imageViewImageDetail;
+    //Model
+    ProductModel productModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
 
-        //Change Text in ActionBar
-        setTitle("Product Name");
+        //Get Views
+        textViewPriceDetail = findViewById(R.id.textViewPriceDetail);
+        textViewDescriptionDetail = findViewById(R.id.textViewDescriptionDetail);
+        imageViewImageDetail = findViewById(R.id.imageViewImageDetail);
+
+        //get the model passed from MainActivity
+        productModel = getIntent().getParcelableExtra("product");
+
+        //set data of model to Views
+        if (productModel != null) {
+            //Product Name to Action Bar
+            setTitle(productModel.getName()); //Change Text in ActionBar
+
+            //Product Price
+            textViewPriceDetail.setText("$ " + productModel.getPrice());
+
+            //Product Description
+            textViewDescriptionDetail.setText(productModel.getDescription());
+
+            //Product Image: Display with Picasso
+            Picasso.get().load(productModel.getImageURL()).into(imageViewImageDetail);
+        }
     }
 }
