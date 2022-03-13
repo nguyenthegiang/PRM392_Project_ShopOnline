8fac74c1eeac0bcbcbd7da95e69849fbd08c5d30
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-13 10:17:40 +0700

    [ManageProductActivity - FrontEnd]: Layout Design

--- app/src/main/java/com/example/prm392_project/ManageProductActivity.java
+++ app/src/main/java/com/example/prm392_project/ManageProductActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ManageProductActvity extends AppCompatActivity {
+public class ManageProductActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_manage_product_actvity);
+        setContentView(R.layout.activity_manage_product);
     }
 }
