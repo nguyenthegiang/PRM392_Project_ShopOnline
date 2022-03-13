db0e208213e2f6aa6235695d8267df1745f560b3
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-13 10:03:29 +0700

    [ManageProductActivity - FrontEnd]: Create activity

---
+++ app/src/main/java/com/example/prm392_project/ManageProductActvity.java
@@ -0,0 +1,14 @@
+package com.example.prm392_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageProductActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_product_actvity);
+    }
+}
