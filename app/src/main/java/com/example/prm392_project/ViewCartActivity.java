d4133260e9b021e1b4c86913e6d9dd80e46ae124
nguyenthegiang <84084582+nguyenthegiang@users.noreply.github.com>
2022-03-14 21:50:07 +0700

    [ViewCartActivity - BackEnd]: Get Cart list from DB

--- app/src/main/java/com/example/prm392_project/ViewCartActivity.java
+++ app/src/main/java/com/example/prm392_project/ViewCartActivity.java
@@ -1,10 +1,13 @@
 package com.example.prm392_project;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -12,6 +15,9 @@ import com.example.prm392_project.Model.CartModel;
 import com.example.prm392_project.Utils.CartRVAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -65,7 +71,53 @@ public class ViewCartActivity extends AppCompatActivity implements CartRVAdapter
         super.onStart();
     }
 
+    //fetch carts data from DB
     private void getCarts() {
+        //clear list
+        cartModelArrayList.clear();
+        //read data from DB
+        cartReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //hide progress bar
+                loadingPB.setVisibility(View.GONE);
+
+                //read data to list: only get Carts of this User
+                CartModel cart = snapshot.getValue(CartModel.class);
+                if (cart.getUserId().equals(userId)) {
+                    cartModelArrayList.add(cart);
+                }
+
+                //notify adapter
+                cartRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //notify adapter & hide progress bar
+                loadingPB.setVisibility(View.GONE);
+                cartRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                //notify adapter & hide progress bar
+                loadingPB.setVisibility(View.GONE);
+                cartRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //notify adapter & hide progress bar
+                loadingPB.setVisibility(View.GONE);
+                cartRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     @Override
